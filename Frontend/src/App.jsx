f98f1122c425af247353d2612d794c079c65e7f8
surajkr20 <suraj71308kumar@gmail.com>
2025-06-03 07:11:27 +0530

    starter page of ui -  done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,17 @@
-import './App.css'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import UserSignup from "./pages/UserSignup";
 
 function App() {
   return (
-    <div className='bg-red-300 text-center'>Hello</div>
-  )
+    <div className="">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user-signup" element={<UserSignup />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
